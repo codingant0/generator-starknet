c9da3adfcb85f4554c155a11ab93cbfb2eff6aab
Antoine <antoine@codingant.io>
2022-09-28 20:47:58 +0200

    test: add basic Hardhat generator test
    It checks that the minimum required files for an Hardhat project are created.

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,15 +1,31 @@
 "use strict";
 const path = require("path");
 const helpers = require("yeoman-test");
+const fs = require("fs");
+const { HARDHAT } = require("../generators/app/constants");
 
-describe("generator-starknet:app", () => {
-  beforeAll(() => {
-    return helpers
+describe("Testing generate Hardhat project", () => {
+  let runResult;
+  beforeAll(async () => {
+    runResult = await helpers
       .run(path.join(__dirname, "../generators/app"))
-      .withPrompts({});
+      .withPrompts({})
+      .inTmpDir(function (dir) {
+        const done = this.async();
+        fs.rename(path.join(__dirname, "test"), dir, done);
+      })
+      .withPrompts({
+        framework: HARDHAT,
+        wantERC20: false,
+        wantCI: false,
+        wantERC721: false,
+      });
   });
 
-  it("creates files", () => {
-    // TODO: implement it
+  it("Should have the minimum required files", () => {
+    runResult.assertFile(".gitignore");
+    runResult.assertFile("hardhat.config.js");
+    runResult.assertFile("package.json");
+    runResult.assertFile("README.md");
   });
 });
